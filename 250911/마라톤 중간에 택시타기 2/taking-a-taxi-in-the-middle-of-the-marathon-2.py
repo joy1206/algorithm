9aae81b301f892068e04b584bb49989b33926071
joy1206 <seoyeon.shin.dev@gmail.com>
2025-09-11 00:47:18 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250911/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 250911/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -1,25 +1,24 @@
 n = int(input())
 points = [tuple(map(int, input().split())) for _ in range(n)]
 
-# 전체 거리 구하기
-total_distance = 0
-for i in range(n-1):
-    dis = abs(points[i][0] - points[i+1][0]) + abs(points[i][1] - points[i+1][1])
-    total_distance += dis
+# i번째 건너 뛰었을때는 i-1번째부터 i+1번째의 거리를 구하면 된다.
 
-# 한 개씩 건너뛰는 모든 경우 
-max_save_distance = 0
-for i in range(1, n-1):
-    # 안 건너 뛴 원래 거리
-    dis = abs(points[i-1][0] - points[i][0]) + abs(points[i-1][1] - points[i][1]) + abs(points[i][0] - points[i+1][0]) + abs(points[i][1] - points[i+1][1])
+answer = 21e8
 
-    # 건너 뛴 거리
-    skipped = abs(points[i-1][0] - points[i+1][0]) + abs(points[i-1][1] - points[i+1][1])
 
-    # 단축량 dis - skipped
-    saved = dis - skipped
-    if max_save_distance < saved:
-        max_save_distance = saved
+for i in range(1, n-1): # 건너뛸 체크포인트를 결정하는 변수
+    dist = 0
+    prev_idx = 0 
+    for j in range(1, n): # 건너뛴 후의 총 거리를 계산하기 위해 모든 체크포인트를 순회하는 변수
+        # 내부 for문에서 건너뛰어야 할 i번째 체크포인트를 제외하고 다음 체크포인트로 바로 넘어가
+        if j == i:
+            continue
+        dist += abs(points[prev_idx][0] - points[j][0]) + abs(points[prev_idx][1] - points[j][1])
+        prev_idx = j
 
-answer = total_distance - max_save_distance
+    answer = min(answer, dist)
+
 print(answer)
+
+
+
