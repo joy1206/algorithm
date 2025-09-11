3c576bc9b68267707fed616e8aa6dc337c5ab13a
joy1206 <seoyeon.shin.dev@gmail.com>
2025-09-12 00:04:23 +0900

    Title: 인접하지 않은 2개의 수, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 250912/인접하지 않은 2개의 수/two-non-adjacent-numbers.py
+++ 250912/인접하지 않은 2개의 수/two-non-adjacent-numbers.py
@@ -7,7 +7,7 @@ for i in range(n-2):
     for j in range(i+2, n):
         num = 0
         num += numbers[i] + numbers[j]
-    answer = max(num, answer)
+        answer = max(num, answer)
 print(answer)
 
 
