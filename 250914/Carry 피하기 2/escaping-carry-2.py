79d38f71a3ce459fa7d5426c70afe5272f53e195
joy1206 <seoyeon.shin.dev@gmail.com>
2025-09-14 23:46:28 +0900

    Title: Carry 피하기 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250914/Carry 피하기 2/escaping-carry-2.py
@@ -0,0 +1,32 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+answer = -21e8
+
+def isCarry(num1, num2, num3):
+    while num1 > 0 or num2 > 0 or num3 > 0:
+        value1 = num1 % 10
+        value2 = num2 % 10
+        value3 = num3 % 10
+
+        if value1 + value2 + value3 >= 10 : 
+            return True
+        
+        num1 //= 10
+        num2 //= 10
+        num3 //= 10
+    return False
+
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(i+2, n):
+            num1 = arr[i]
+            num2 = arr[j]
+            num3 = arr[k]
+            if not isCarry(num1, num2, num3):
+                sum = num1 + num2 + num3
+                answer = max(answer, sum)
+
+print(answer)
+
+ 
