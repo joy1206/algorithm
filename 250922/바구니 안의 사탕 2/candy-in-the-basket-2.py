50bcd364a6036f7b59767aab12ab8a77a69b1b47
joy1206 <seoyeon.shin.dev@gmail.com>
2025-09-22 01:02:09 +0900

    Title: 바구니 안의 사탕 2, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250922/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -0,0 +1,22 @@
+N, K = map(int, input().split())
+max_end = 101
+locations = {}
+arr = [0] * max_end
+answer = 0
+
+for _ in range(N):
+    candy, pos = map(int, input().split())
+    locations[pos] = candy
+
+for pos, candy in locations.items():
+    arr[pos] = candy
+
+for c in range(max_end):
+    if c - K > 0 and c + K < max_end:
+        temp = 0
+        for i in range(c-K, c+K+1):
+            temp += arr[i]
+        if temp >answer:
+            answer = temp
+print(answer)
+
