dc7baf8d9bbf8ded9c7acda96eded2417f73bda1
joy1206 <seoyeon.shin.dev@gmail.com>
2025-09-22 11:03:59 +0900

    Title: 개발자의 능력 3, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250922/개발자의 능력 3/ability-of-developer-3.py
+++ 250922/개발자의 능력 3/ability-of-developer-3.py
@@ -1,12 +1,13 @@
 abilities = list(map(int, input().split()))
 
-def get_diff(i, j):
-    sum1 = abilities[i] + abilities[j]
+def get_diff(i, j, k):
+    sum1 = abilities[i] + abilities[j] + abilities[k]
     sum2 = sum(abilities)- sum1
     return abs(sum1-sum2)
 
 min_diff = 21e8
 for i in range(0, 6):
     for j in range(i+1, 6):
-        min_diff = min(min_diff, get_diff(i, j))
+        for k in range(j+1, 6):
+            min_diff = min(min_diff, get_diff(i, j, k))
 print(min_diff)
