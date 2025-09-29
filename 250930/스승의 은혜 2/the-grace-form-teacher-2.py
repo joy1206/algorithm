beff43421672f10e1ea922e47a28376daa1b4687
joy1206 <seoyeon.shin.dev@gmail.com>
2025-09-30 00:59:12 +0900

    Title: 스승의 은혜 2, Time: 60ms, Memory: 17MB, Status: Passed - Codetree

--- 250930/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 250930/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -1,19 +1,22 @@
 N, B = map(int, input().split())
 P = [int(input()) for _ in range(N)]
-
+P.sort()
 answer = 0
 
 for i in range(N):
-    student = 0
     coupon = P[i] // 2
     temp_B = B - coupon
+
+    if temp_B < 0:
+        continue
+    student = 1
     for j in range(N):
         if i == j: continue
-        tempB = B - P[j]
+        temp_B -= P[j]
         if temp_B < 0:
             break
         else:
             student +=1
     answer = max(answer, student)
 
-print(student)
+print(answer)
