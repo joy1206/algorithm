ca2b6dc7865269f3dfdaffa5dbd57ffe7f809a2e
joy1206 <seoyeon.shin.dev@gmail.com>
2025-10-05 00:30:44 +0900

    Title: 이상한 폭탄 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251005/이상한 폭탄 2/strange-bomb-2.py
@@ -0,0 +1,15 @@
+N, K = map(int, input().split())
+num = [int(input()) for _ in range(N)]
+
+ans = -1 
+
+# 폭탄 쌍 i, j
+for i in range(N):
+    for j in range(i+1, N):
+        if num[i] == num[j]:
+        # j - i가 K 이내인지 확인
+            if j - i <= K:
+                temp = max(num[i:j+1])
+                ans = max(ans, temp)
+
+print(ans)
