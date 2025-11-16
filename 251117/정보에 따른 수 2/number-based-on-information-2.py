a66e9ba6b88137d1f060b02111dfc83e10d62c8e
joy1206 <seoyeon.shin.dev@gmail.com>
2025-11-17 00:00:19 +0900

    Title: 정보에 따른 수 2, Time: 86ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 251117/정보에 따른 수 2/number-based-on-information-2.py
@@ -0,0 +1,24 @@
+T, a, b = map(int, input().split())
+c = []
+x = []
+ans = 0
+for _ in range(T):
+    char, pos = input().split()
+    c.append(char)
+    x.append(int(pos))
+
+length = max(x)
+
+for i in range(1, length +1):
+    d1, d2 = 1000, 1000
+    for j in range(T):
+        if c[j] == 'S':
+            temp = abs(x[j]-i)
+            d1 = min(d1, temp)
+        else:
+            temp = abs(x[j]-i)
+            d2 = min(d2, temp)
+    if d1 <= d2: 
+        ans +=1
+
+print(ans)
