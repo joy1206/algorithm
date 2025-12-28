e0b2c3ea152a2c77bd48b7f2911e6aaa4de9ddf6
joy1206 <seoyeon.shin.dev@gmail.com>
2025-12-28 23:56:09 +0900

    Title: 최대 최소간의 차, Time: 65ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 251228/최대 최소간의 차/difference-between-maximum-and-minimum.py
+++ 251228/최대 최소간의 차/difference-between-maximum-and-minimum.py
@@ -1,19 +1,16 @@
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 ans = 21e8
-current_cost = 0
-while ans > current_cost :
-    for i in range(n):
-        max_arr = max(arr)
-        min_arr = min(arr)
-        if arr[i] == max_arr:
-            arr[i] -= 1
-            current_cost +=1
-        elif arr[i] == min_arr:
-            arr[i] += 1
-            current_cost += 1
+# 최소부터 최대까지 a, a+k
+min_num = min(arr)
+max_num = max(arr)
+for i in range(min_num, max_num+1):
+    current_cost = 0
+    for j in range(n):
+        num_cost = min(abs(arr[j]-i), abs(arr[j]-(i+k)))
+        current_cost += num_cost
+    if current_cost < ans:
         ans = current_cost
 
 print(ans)
-
     
