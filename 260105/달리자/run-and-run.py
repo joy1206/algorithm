cc4b36b6b7206801d57ac877ae009822a2302d79
joy1206 <seoyeon.shin.dev@gmail.com>
2026-01-05 23:34:08 +0900

    Title: 달리자, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260105/달리자/run-and-run.py
@@ -0,0 +1,11 @@
+n = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+ans = 0
+
+for i in range(n):
+    if A[i] > B[i]:
+        A[i+1] += A[i] - B[i]
+        ans += A[i] - B[i]
+
+print(ans)
