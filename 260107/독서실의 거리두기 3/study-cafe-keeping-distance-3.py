36de3f8c21389b98ca89173e85cd21871ae97282
joy1206 <seoyeon.shin.dev@gmail.com>
2026-01-07 23:12:36 +0900

    Title: 독서실의 거리두기 3, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 260107/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
+++ 260107/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -4,15 +4,12 @@ seats = input()
 # 가장 먼 1간의 쌍 찾기
 min_dist = 21e8
 max_dist = 0
-left, right = 0, 0
 for i in range(N):
     if seats[i] == '1':
         for j in range(i+1, N):
             if seats[j] == '1':
                 max_dist = max(max_dist, j-i)
                 min_dist = min(min_dist, j-i)
-                left = i
-                right = j
                 break
 
 if min_dist == 1:
