47512bcefb15a6aaf290a751f09d975cc04ee7e8
joy1206 <seoyeon.shin.dev@gmail.com>
2026-01-14 22:26:28 +0900

    Title: 순위 경쟁, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 260114/순위 경쟁/ranking-competition.py
+++ 260114/순위 경쟁/ranking-competition.py
@@ -1,23 +1,22 @@
 n = int(input())
-stage = []
+stage = ['A', 'B', 'C']
 scores = {'A': 0, 'B': 0, 'C': 0}
 cnt = 0
 
 arr = [tuple(input().split()) for _ in range(n)]
 
-if len(arr) == 1:
-    cnt = 0
+ 
+for person, score in arr:
+    scores[person] += int(score)
+    max_score = max(scores.values())
+    top_players = []
+        
+    for p in ['A', 'B', 'C']:
+        if scores[p] == max_score:
+            top_players.append(p)
 
-else: 
-    for person, score in arr:
-        scores[person] += int(score)
-        max_score = max(scores.values())
-        top_players = []
-        for key, value in scores.items():
-            if scores[key] == max_score:
-                top_players.append(key)
-        if stage != top_players:
-            cnt += 1
-            stage = top_players
+    if stage != top_players:
+        cnt +=1
+        stage= top_players
 
 print(cnt)
