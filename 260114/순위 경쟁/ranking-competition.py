18d0f43363bd2f03687dc62e5c55d9766427d3a6
joy1206 <seoyeon.shin.dev@gmail.com>
2026-01-14 22:19:49 +0900

    Title: 순위 경쟁, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260114/순위 경쟁/ranking-competition.py
+++ 260114/순위 경쟁/ranking-competition.py
@@ -5,15 +5,19 @@ cnt = 0
 
 arr = [tuple(input().split()) for _ in range(n)]
 
-for person, score in arr:
-    scores[person] += int(score)
-    max_score = max(scores.values())
-    top_players = []
-    for key, value in scores.items():
-        if scores[key] == max_score:
-            top_players.append(key)
-    if stage != top_players:
-        cnt += 1
-        stage = top_players
+if len(arr) == 1:
+    cnt = 0
+
+else: 
+    for person, score in arr:
+        scores[person] += int(score)
+        max_score = max(scores.values())
+        top_players = []
+        for key, value in scores.items():
+            if scores[key] == max_score:
+                top_players.append(key)
+        if stage != top_players:
+            cnt += 1
+            stage = top_players
 
 print(cnt)
