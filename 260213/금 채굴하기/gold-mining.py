de8d9536cb3ea690463487e03585a9a628a24613
joy1206 <seoyeon.shin.dev@gmail.com>
2026-02-13 23:50:40 +0900

    Title: 금 채굴하기, Time: 79ms, Memory: 18MB, Status: Passed - Codetree

---
+++ 260213/금 채굴하기/gold-mining.py
@@ -0,0 +1,26 @@
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+def solution(r, c, k):
+    count = 0
+    for i in range(n):
+        for j in range(n):
+            if abs(r-i) + abs(c-j) <= k:
+                if grid[i][j] == 1:
+                    count +=1
+    return count
+
+max_gold = 0
+
+for r in range(n):
+    for c in range(n):
+        for k in range(2 * n + 1):
+            gold_cnt = solution(r, c, k)
+            cost = k*k + (k+1) * (k+1)
+            profit = gold_cnt * m - cost
+            
+            if profit >= 0:
+                if gold_cnt > max_gold:
+                    max_gold = gold_cnt
+
+print(max_gold)
