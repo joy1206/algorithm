25e383a1443e0bff6f407ad06a55530cae1becf6
joy1206 <seoyeon.shin.dev@gmail.com>
2026-02-19 23:32:16 +0900

    Title: 컨베이어 벨트, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260219/컨베이어 벨트/conveyor-belt.py
@@ -0,0 +1,8 @@
+n, t = map(int, input().split())
+arr = list(map(int, input().split())) + list(map(int, input().split()))
+
+for _ in range(t):
+    arr = [arr[-1]] + arr[:-1]
+
+print(*arr[:n])
+print(*arr[n:])
